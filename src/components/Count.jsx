bd25f861a09b42d7ce216d36061c51b903429eb5
rahuldhiman1313 <rahuldhiman521@gmail.com>
2024-04-14 19:28:50 +0530

    done

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 function Count() {
   const initialCount = 2100000; // Initial count value
+  const incrementValue = 300; // Increment value
+  const intervalDuration = 2000; // Duration between increments in milliseconds
+
   const [count, setCount] = useState(() => {
     const storedCount = localStorage.getItem('count');
     return storedCount ? parseInt(storedCount, 10) : initialCount;
@@ -10,14 +13,14 @@ function Count() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCount(prevCount => {
-        const newCount = prevCount + 1;
+        const newCount = prevCount + incrementValue;
         localStorage.setItem('count', newCount.toString()); // Update count in localStorage
         return newCount;
       });
-    }, 5000); // Increase count every second (1000 milliseconds)
+    }, intervalDuration);
     
     return () => clearInterval(intervalId); // Clean up the interval when the component unmounts
-  }, []); // Empty dependency array ensures the effect runs only once on component mount
+  }, []);
 
   return (
     <div>
